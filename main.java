22a139e3b5c87f21406ab50260134e5d6042c626
Tanushree227 <tanushreedas813@gmail.com>
2025-06-29 21:40:04 +0530

    practising java

---
+++ main.java
@@ -0,0 +1,8 @@
+import practise.hello;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hakuna Matata");
+        System.out.println(hello.x);
+    }
+}
---
+++ practise/hello.java
@@ -0,0 +1,8 @@
+package practise;
+
+public class hello {
+    public static int x = 2207;
+    public static void main(String[] args) {
+        System.out.println(x);
+    }
+}
