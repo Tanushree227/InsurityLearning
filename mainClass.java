835fcf69681f348865c4430b78be25ee4db0b4e5
Tanushree227 <tanushreedas813@gmail.com>
2025-06-30 22:55:18 +0530

    Task 3

--- mainClass.java
+++ mainClass.java
@@ -4,5 +4,6 @@ public class mainClass {
         System.out.println("To Do List: ");
         System.out.println("1. Find nice place to stay.");
         System.out.println("2. Start having proper diet");
+        System.out.println("3. Live life to the fullest");
     }
 }
