6368745288b740cd90429c4a6f0e8ad445dcc6ac
Tanushree227 <tanushreedas813@gmail.com>
2026-02-03 16:22:27 +0530

    CSR: 4398489

--- mainClass.java
+++
@@ -1,9 +0,0 @@
-public class mainClass {
-    public static void main(String[] args) {
-        System.out.println("Hakuna Matata");
-        System.out.println("To Do List: ");
-        System.out.println("1. Find nice place to stay.");
-        System.out.println("2. Start having proper diet");
-        System.out.println("3. Live life to the fullest");
-    }
-}
